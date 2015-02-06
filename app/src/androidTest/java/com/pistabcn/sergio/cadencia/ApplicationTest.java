0264a43ecfc6085c5b9a9cac4d55765db5bc1132
Sergio Romero <sergio.romero.garcia@gmail.com>
2015-02-06 12:49:37 +0100

    Tercero

--- app/src/androidTest/java/com/pistabcn/sergio/cadencia/ApplicationTest.java
+++ app/src/androidTest/java/com/pistabcn/sergio/cadencia/ApplicationTest.java
@@ -5,6 +5,7 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Sergio Romero
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
